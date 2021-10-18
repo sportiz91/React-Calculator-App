204d53c552b3bf45b57eefc411dcd1e04a3b5c95
Lasantoneta <sportiz9109@gmail.com>
2021-10-18 14:36:15 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+## React Calculator App
+
+This is my second App built with React. It's done with simple `create-react-app`. For more information about React checkout docs: [React Docs](https://es.reactjs.org/docs/getting-started.html).
+
+## Table of Contents
+
+- [Quick Start](#quick-start)
+- [Next Steps](#next-steps)
+- [Thanks](#thanks)
+- [Learnings](#learnings)
+
+## Quick Start
+
+1. Init a new repo: `git init`.
+1. Clone the repo: `git clone TBD`.
+1. CD over `react-app-santi` folder.
+1. Install with npm: `npm install`.
+1. Run in locally on your machine: `npm start`.
+
+## Next Steps
+
+1. Define smaller components.
+1. Mobile view.
+1. Host in Heroku.
+1. Update files.
+
+## Thanks
+
+This todo-app was possible thanks to Tomy from tomykelly100 YouTube Channel. Please checkout his content: [Tomy YouTube Channel](https://www.youtube.com/channel/UCBeE2qKffzEzRoWns6RZ8UA/videos)
+
+## Learnings
+
+1. More practice into React useState, useEffect & components.
+1. CSS: more practice into Grid System. Pretty comfy using this style of layout by now.
+1. Infraestructure: more practice into Git, MarkDown & hosting into Heroku.
+1. Learnt about different use cases for "?" keyword. More specifically:
+
+   - Ternary operator
+   - Optional Chaining -> in this App we use Optional Chaining because we have a function `equals()` that gets called under certain conditions and can be triggered by an event listener too. In the case that the function is executed instead of triggered, then we would have an error. With `?` we can save this scenario.
+   - Nullish Coalescing
+
+1. Learnt about NumberFormat library. Please checkout: [NumberFormat](https://www.npmjs.com/package/react-number-format).
---
+++ src/App.js
@@ -0,0 +1,203 @@
+import React, { useState, useEffect } from "react";
+import NumberFormat from "react-number-format";
+
+function App() {
+  //State Variables:
+  const [show, setShow] = useState("0");
+  const [prevNumber, setPrevNumber] = useState("");
+  const [currentNumber, setCurrentNumber] = useState("");
+  const [operation, setOperation] = useState(null);
+  const [total, setTotal] = useState(false);
+
+  //Effct Hooks:
+  useEffect(() => {
+    setShow(currentNumber);
+  }, [currentNumber]);
+
+  useEffect(() => {
+    setShow("0");
+  }, []);
+
+  //Functions:
+  const handleNumber = (e) => {
+    if (e.target.innerText === "." && currentNumber.includes(".")) return;
+
+    if (currentNumber === "") {
+      setCurrentNumber(e.target.innerText);
+    } else if (currentNumber.charAt(0) === "0") {
+      setCurrentNumber((acumulatedString) => {
+        return acumulatedString.substr(1) + e.target.innerText;
+      });
+    } else {
+      setCurrentNumber((acumulatedString) => {
+        return acumulatedString + e.target.innerText;
+      });
+    }
+  };
+
+  const operate = (e) => {
+    console.log(e.target.innerText);
+
+    if (currentNumber === "" || currentNumber === "0") return;
+
+    if (prevNumber === "") {
+      setPrevNumber(currentNumber);
+      setCurrentNumber("");
+      setOperation(e.target.innerText);
+    } else {
+      equals(); //Here we are applying function equals. The case is different when function get's triggered by an event listener. In this case,
+      //e.target will be undefined. In order to save this case, we need to use the e? operator. This is called Optional Chaining and is the second
+      //Use case for ? (the other ones are: Ternary Operator & Nullish Coalescing)
+      setOperation(e.target.innerText);
+    }
+  };
+
+  const equals = (e) => {
+    if (currentNumber === "" || currentNumber === "0" || prevNumber === "")
+      return;
+
+    if (e?.target === undefined || e?.target !== undefined) {
+      const path = e?.target;
+      let result;
+      switch (operation) {
+        case "+":
+          result = parseFloat(prevNumber) + parseFloat(currentNumber);
+          break;
+        case "-":
+          result = parseFloat(prevNumber) - parseFloat(currentNumber);
+          break;
+        case "X":
+          result = parseFloat(prevNumber) * parseFloat(currentNumber);
+          break;
+        case "/":
+          result = parseFloat(prevNumber) / parseFloat(currentNumber);
+          break;
+        default:
+          return;
+      }
+
+      if (path === undefined) {
+        setPrevNumber(String(result));
+        setCurrentNumber("");
+      } else {
+        setCurrentNumber(String(result));
+        setPrevNumber("");
+      }
+    }
+  };
+
+  const handleAllClear = () => {
+    setCurrentNumber("0");
+    setPrevNumber("");
+    setOperation(null);
+  };
+
+  const minusPlus = () => {
+    if (currentNumber.charAt(0) === "0" || currentNumber.charAt(0) == "")
+      return;
+
+    if (currentNumber.charAt(0) === "-") {
+      setCurrentNumber((acumulatedString) => {
+        return acumulatedString.substr(1);
+      });
+    } else {
+      setCurrentNumber((acumulatedString) => {
+        return "-" + acumulatedString;
+      });
+    }
+  };
+
+  const applyPercentage = () => {
+    if (currentNumber === "" || currentNumber === "0") return;
+
+    if (prevNumber === "") {
+      setCurrentNumber((acumulatedString) => {
+        return acumulatedString / 100;
+      });
+    } else {
+      setCurrentNumber((acumulatedString) => {
+        return parseFloat(prevNumber) * (parseFloat(currentNumber) / 100);
+      });
+    }
+  };
+
+  return (
+    <div className="wrapper">
+      <div className="display">
+        <NumberFormat
+          displayType="text"
+          value={show}
+          thousandSeparator={true}
+        />
+      </div>
+
+      <button className="ac btn" onClick={handleAllClear}>
+        AC
+      </button>
+      <button className="percentage btn" onClick={applyPercentage}>
+        %
+      </button>
+      <button className="minus btn" onClick={minusPlus}>
+        +/-
+      </button>
+
+      <button className="operand btn" onClick={operate}>
+        /
+      </button>
+
+      <button className="number btn" onClick={handleNumber}>
+        7
+      </button>
+      <button className="number btn" onClick={handleNumber}>
+        8
+      </button>
+      <button className="number btn" onClick={handleNumber}>
+        9
+      </button>
+
+      <button className="operand btn" onClick={operate}>
+        X
+      </button>
+      <button className="number btn" onClick={handleNumber}>
+        4
+      </button>
+      <button className="number btn" onClick={handleNumber}>
+        5
+      </button>
+      <button className="number btn" onClick={handleNumber}>
+        6
+      </button>
+
+      <button className="operand btn" onClick={operate}>
+        +
+      </button>
+
+      <button className="number btn" onClick={handleNumber}>
+        1
+      </button>
+      <button className="number btn" onClick={handleNumber}>
+        2
+      </button>
+      <button className="number btn" onClick={handleNumber}>
+        3
+      </button>
+
+      <button className="operand btn" onClick={operate}>
+        -
+      </button>
+
+      <button className="number zero btn" onClick={handleNumber}>
+        0
+      </button>
+
+      <button className="number btn" onClick={handleNumber}>
+        .
+      </button>
+      <button className="equal btn" onClick={equals}>
+        =
+      </button>
+    </div>
+  );
+}
+
+export default App;
